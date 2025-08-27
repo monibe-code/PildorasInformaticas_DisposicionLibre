4ef419c818ca1b8bf824ca6fb561aafca0ea0589
Monica <hola.monibe@gmail.com>
2025-08-27 07:37:02 +0200

    Añadida clase que implementa LayoutManager y programado el método layoutContainer

--- src/libreDisposicion/LibreDisposicion.java
+++ src/libreDisposicion/LibreDisposicion.java
@@ -1,5 +1,7 @@
 package libreDisposicion;
 
+import java.awt.*;
+
 import javax.swing.*;
 
 public class LibreDisposicion {
@@ -24,20 +26,69 @@ class MarcoLibre extends JFrame{
 class LaminaLibre extends JPanel{
 	public LaminaLibre() {
 		//establecemos una disposición libre
-		setLayout(null);
+		setLayout(new EnColumnas());
 		JLabel nombre= new JLabel("Nombre");
 		JLabel apellido= new JLabel("Apellido");
 		JTextField c_nombre= new JTextField();
 		JTextField c_apellido= new JTextField();
-		nombre.setBounds(20,20,80,10);
-		c_nombre.setBounds(100, 17, 100, 20);
-		apellido.setBounds(20,60,80,15);
-		c_apellido.setBounds(100, 55, 100, 20);
+		JLabel edad= new JLabel("Edad");
+		JTextField c_edad= new JTextField();
+
 		add(nombre);
-		add(apellido);
 		add(c_nombre);
+		add(apellido);
 		add(c_apellido);
+		add(edad);
+		add(c_edad);
 		
 		
 	}
+}
+
+class EnColumnas implements LayoutManager{
+
+	@Override
+	public void addLayoutComponent(String name, Component comp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void removeLayoutComponent(Component comp) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public Dimension preferredLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Dimension minimumLayoutSize(Container parent) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void layoutContainer(Container micontenedor) {
+		// TODO Auto-generated method stub
+		int contador=0;
+		int n= micontenedor.getComponentCount();
+		for (int i=0; i<n;i++) {
+			contador++;
+			Component c= micontenedor.getComponent(i);
+			c.setBounds(x, y, 100, 20);
+			x+=100;
+			if (contador%2==0) {
+				x=20;
+				y+=40;
+			}
+		}
+	}
+	
+	private int x=20;
+	private int y=20;
+	
 }
